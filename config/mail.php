c049994518b25e86dc6c8a0f62e22ccafd6ae1b1
Rob Taylor <rob@uxblondon.com>
2016-01-19 18:10:00 +0000

    Removed the default from email details from config/mail.php
    This needs to be overriden by the mail config file in boomcms/boom-core so that details are set from site settings

--- config/mail.php
+++ config/mail.php
@@ -45,19 +45,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Global "From" Address
-    |--------------------------------------------------------------------------
-    |
-    | You may wish for all e-mails sent by your application to be sent from
-    | the same address. Here, you may specify a name and address that is
-    | used globally for all e-mails that are sent by your application.
-    |
-    */
-
-    'from' => ['address' => null, 'name' => null],
-
-    /*
-    |--------------------------------------------------------------------------
     | E-Mail Encryption Protocol
     |--------------------------------------------------------------------------
     |
