25196189684809f65de6029b9936166639ec813d
Rob Taylor <rob@uxblondon.com>
2016-12-19 13:01:20 +0000

    Added NotificationServiceProvider to config/app.php

--- config/app.php
+++ config/app.php
@@ -144,6 +144,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Illuminate\Notifications\NotificationServiceProvider::class,
 
         // BoomCMS service providers
         BoomCMS\ServiceProviders\RobotsServiceProvider::class,
